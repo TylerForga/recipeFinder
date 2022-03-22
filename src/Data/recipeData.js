7e69de1f51a8e8e3447359ebc4a5ca2d60c5b1bf
Tyler Forga <forgatyler@gmail.com>
2022-03-22 17:58:08 -0500

    decluttered and deleted unused files

--- src/Data/recipeData.js
+++
@@ -1,9 +0,0 @@
-const recipeData = [
-{
-    name: "Chicken Alfredo",
-    ingredients: "Chicken, Pasta, Alfredo Sauce",
-    calories: "919",
-    cookTime: "33 Minutes",
-    recipeType: "Pasta"
-}, 
-]
